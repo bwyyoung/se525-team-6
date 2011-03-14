088eeaeb5c8990beba89d1a018fce6c725076cd9
adam.gojdas <adam.gojdas@gmail.com>
2011-03-14 12:23:34 +0000

    updating latest ocde

---
+++ src/se525/team6/ChallengeResponseItemAdapter.java
@@ -0,0 +1,52 @@
+package se525.team6;
+/* Class to use as an adapter to work with challenge/response pairs to be presented in gui
+ * a. gojdas
+*/
+
+import android.content.Context;
+import java.util.*;
+import android.view.*;
+import android.widget.*;
+import se525.team6.R;
+
+public class ChallengeResponseItemAdapter extends
+		ArrayAdapter<ChallengeResponseItem> {
+
+	private int _resource;
+	
+	
+	public ChallengeResponseItemAdapter(Context context,
+			int textViewResourceId, List<ChallengeResponseItem> objects) {
+		super(context, textViewResourceId, objects);
+		_resource = textViewResourceId;
+	}
+
+	  @Override
+	  public View getView(int position, View convertView, ViewGroup parent) {
+	    LinearLayout challengeResponseView;
+
+	    ChallengeResponseItem item = getItem(position);
+
+	    String challenge = item.getChallenge();
+	    String response = item.getResponse();
+
+	    if (convertView == null) {
+	      challengeResponseView = new LinearLayout(getContext());
+	      String inflater = Context.LAYOUT_INFLATER_SERVICE;
+	      LayoutInflater vi = (LayoutInflater)getContext().getSystemService(inflater);
+	      vi.inflate(_resource, challengeResponseView, true);
+	    } else {
+	      challengeResponseView = (LinearLayout) convertView;
+	    }
+
+	    TextView challengeView = (TextView)challengeResponseView.findViewById(R.id.TextView01);
+	    TextView responseView = (TextView)challengeResponseView.findViewById(R.id.TextView02);
+	      
+	    challengeView.setText(challenge);
+	    responseView.setText(response);
+
+	    return challengeResponseView;
+	  }
+	
+	
+}
--- src/se525/team6/Primary.java
+++ src/se525/team6/Primary.java
@@ -1,13 +1,29 @@
 package se525.team6;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Primary extends Activity {
+   private Button _myButton01;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //adg added button to launch the challenge response adding screen
+        _myButton01 = (Button)findViewById(R.id.Button01);
+        
+        // capture onclick of button to do processing
+        _myButton01.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {          
+              //launch the challenge response activity;
+              Intent intent = new Intent(Primary.this, SmsActivity.class);
+              startActivity(intent);
+           }
+        });
     }
 }
